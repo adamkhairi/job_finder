7696c17d4d93708619c6cbad897ef7c74f130680
Adam Khairi <khairiadam2@gmail.com>
2024-11-27 05:42:42 +0100

    updates

--- src/app/job/services/jobs-search.service.ts
+++ src/app/job/services/jobs-search.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { environment } from "../../../environment/environment";
+import {environment} from '../../../environments/environment';
 
 export interface SearchResponse {
   items: any[];
