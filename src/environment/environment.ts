9fdff58198c4310bcfc41975bfc615adc5fdc21d
Adam Khairi <57996941+adamkhairi@users.noreply.github.com>
2024-11-27 05:39:33 +0100

    Delete src/environment/environment.ts

--- src/environment/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  GOOGLE_API_KEY: "AIzaSyDaN0meg3q_FJCDa1w3R66eN0Al2xzwrsI",
-  GOOGLE_SEARCH_ENGINE_ID: "02607133b59a049ba", //"715c88af286c7448a",
-};
